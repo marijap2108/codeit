7f05b7a2f6812cc970b8aea1ac492d18749e2ad4
rakuncina <rasaradoslav@gmail.com>
2021-09-12 16:26:52 +0200

    database connection

--- pages/api/user/createUser.js
+++ pages/api/user/createUser.js
@@ -1,6 +1,11 @@
 import { checkUser } from "../../../utils/checkUser"
+import {connectToDatabase} from "../../../utils/mongodb"
 
-export default (req, res) => {
+export default async (req, res) => {
+  const {db} = await connectToDatabase()
+  const data = await db.collection("codeIt").find({}).toArray()
+
+  return res.status(200).json(data)
   if (checkUser(req)) {
     return res.status(400).json({})
   }
