d503086ae83b50dc5310618d5b33b44e2cf4d020
Marija-Petrovic-IT69-2019 <marry.sugar97@gmail.com>
2021-10-05 15:41:53 +0200

    final commit

--- components/pages/NotLoggedIn.jsx
+++ components/pages/NotLoggedIn.jsx
@@ -1,10 +1,11 @@
-import {NavBar} from '../uiComponents'
+import {Card, NavBar} from '../uiComponents'
 import styles from './NotLoggedIn.module.scss'
 import Image from 'next/image'
+import { FiMail, FiMapPin, FiPhone } from 'react-icons/fi'
 
 export const NotLoggedIn = () => {
   return (
-    <div className={styles.home}>
+    <div id="home" className={styles.home}>
       <NavBar />
       <section className={styles.about}>
         <div className={styles.aboutLeft}>
@@ -16,6 +17,7 @@ export const NotLoggedIn = () => {
         </div>
       </section>
       <section className={styles.features}>
+        <h2 id="features">Features</h2>
         <div className={styles.feature}>
           <div className={styles.left}>
             <Image src="/groups.png" width="auto" height="auto" />
@@ -43,13 +45,35 @@ export const NotLoggedIn = () => {
             <Image src="/savedPosts.png" width="auto" height="auto" />
           </div>
           <div className={styles.right}>
-            <h2>Save posts for later user</h2>
+            <h2>Save posts for later use</h2>
             <p>
               Have you find some posts that will be helpful later, Just Save It, you can save all posts and see them later in your profile
             </p>
           </div>
         </div>
       </section>
+      <section id="contact" className={styles.contact}>
+        <Card className="contactCard">
+          <h2>Contact information</h2>
+          <p>
+            Contact us for any help and suggestions, our team is available 24h.
+          </p>
+          <span><FiPhone /> +817 467346</span>
+          <span><FiMail /> support@codeit.com</span>
+          <span><FiMapPin /> 102 Street 4511 Mike</span>
+        </Card>
+      </section>
+      <footer className={styles.footer}>
+        <span>
+          <a href="#home">
+            <Image src="/logo.svg" width={120} height={30} />
+          </a> 
+          created by Marija Petrovic IT41
+        </span>
+        <span>
+          Copyright © 2021–2022 Codeit
+        </span>
+      </footer>
     </div>
   )
 }
--- components/uiComponents/NavBar/NavBar.jsx
+++ components/uiComponents/NavBar/NavBar.jsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import Image from 'next/image'
 import {Button} from '../Button'
 import styles from './NavBar.module.scss'
@@ -28,11 +27,12 @@ export const NavBar = () => {
 	return (
 		<nav className={styles.navBar}>
 			<span className={styles.left}>
-				<Image src="/logo.svg" width={120} height={30} />
+				<a href="#home">
+					<Image src="/logo.svg" width={120} height={30} />
+				</a>
 				<span>
-					<Link href='/'><Button variant="textual">About</Button></Link>
-					<Link href='/'><Button variant="textual">Plan</Button></Link>
-					<Link href='/'><Button variant="textual">Contact</Button></Link>
+					<a href='#features'><Button variant="textual">Features</Button></a>
+					<a href='#contact'><Button variant="textual">Contact</Button></a>
 				</span>
 			</span>
 			<span className={styles.right}>
